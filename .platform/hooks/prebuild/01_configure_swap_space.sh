e41ea0fab25c13a55e185bb3248774d45d5a3e56
ChungIl <cnddlf12@naver.com>
2023-08-13 03:42:37 +0900

    swapfile 오류 해결

--- .platform/hooks/prebuild/01_configure_swap_space.sh
+++ .platform/hooks/prebuild/01_configure_swap_space.sh
@@ -2,12 +2,12 @@
 
 SWAPFILE=/var/swapfile
 
-if [ -f #SWAPFILE ]; then
+if [ -f $SWAPFILE ]; then
     echo "$SWAPFILE found, skip"
     exit;
 fi
 
-/bin/dd if=/dev/zero of=#SWAPFILE bs=1M count=1024
+/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=1024
 /bin/chmod 600 $SWAPFILE
 /bin/mkswap $SWAPFILE
 /bin/swapon $SWAPFILE
