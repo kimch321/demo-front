cc5adeca65fa8ae3b29cd109dd406f405a87a354
ChungIl <cnddlf12@naver.com>
2023-08-12 15:16:42 +0900

    (5-5) 리다이렉트 적용

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     ListItem,
     ListItemText,
@@ -16,13 +16,12 @@ const Todo = (props) => {
     const editItem = props.editItem;
 
     const editEventHandler = (e) => {
-        item.title = e.target.value;
-        editItem();
+        setItem({...item, title: e.target.value});
     };
 
     const checkboxEventHandler = (e) => {
         item.done = e.target.checked;
-        editItem();
+        editItem(item);
     }
 
     // deleteEventHandler 작성
@@ -36,8 +35,9 @@ const Todo = (props) => {
 
     // turnOnReadOnly 함수 작성
     const turnOnReadOnly = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === "Enter" && readOnly === false) {
             setReadOnly(true);
+            editItem(item);
         }
     };
 
