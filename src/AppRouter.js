f1313713a20a39e701ba9ea089428e271b699eb6
ChungIl <cnddlf12@naver.com>
2023-08-12 15:10:34 +0900

    (5-4) 라우팅

---
+++ src/AppRouter.js
@@ -0,0 +1,33 @@
+import React from "react"
+import "./index.css"
+import {Box, Typography} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import App from "./App";
+import Login from "./Login"
+
+function Copyright() {
+    return(
+        <Typography varian="body2" color="textSecondary" align="center">
+            {"Copyright ©"}
+            fsoftwareengineer, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+
+export default function AppRouter() {
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="login" element={<login />} />
+                </Routes>
+            </BrowserRouter>
+            <Box mt={5}>
+                <Copyright />
+            </Box>
+        </div>
+    );
+}
