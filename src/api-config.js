c7bc7df8e30f49885451a9bc4fb569e16944a109
ChungIl <cnddlf12@naver.com>
2023-08-13 14:46:40 +0900

    http -> https

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
 } else {
-    backendHost = "http://api.kimch321project.com";
+    backendHost = "https://api.kimch321project.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
