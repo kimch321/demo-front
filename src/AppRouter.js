0313bb56f9d0cdf977885f1adaed58e5e9ef955f
ChungIl <cnddlf12@naver.com>
2023-08-13 13:33:40 +0900

    에러 긴급 수정

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -22,8 +22,7 @@ export default function AppRouter() {
         <div>
             <BrowserRouter>
                 <Routes>
-                    {/*<Route path="/" element={<App />} />*/}
-                    <Route path="/" element={<Login />} />
+                    <Route path="/" element={<App />} />
                     <Route path="login" element={<Login />} />
                     <Route path="signup" element={<SignUp />} />
                 </Routes>
